748c5861954061e457b774bdb1e94fe29eac6db1
cp-irona <chandraprakash@irona.ai>
2025-02-28 12:01:31 +0530

    supporting models are loaded from gist

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const IRONAAI_API_KEY_PREFIX = "sk_";
 export class IronaAI {
   private ironaRouter: IronaRouterClient;
   private llmChatService: IronaChatClient;
-  constructor(config: Config = {}) {
+  private constructor(config: Config = {}) {
     const apiKey = config.apiKey || process.env.IRONAAI_API_KEY;
     if (!apiKey) {
       throw new MissingApiKeyError(
@@ -29,10 +29,35 @@ export class IronaAI {
         "The provided API key is invalid. Please generate a new key at 'https://app.irona.ai/dashboard/api-keys'."
       );
     }
-    updateProvidersFromGist();
     config.baseUrl = config?.baseUrl || DEFAULT_BASE_URL;
     this.ironaRouter = new IronaRouterClient(config);
     this.llmChatService = new IronaChatClient(config, this.ironaRouter);
+  }
+  // Static factory method to handle async initialization
+  public static async createInstance(config: Config = {}): Promise<IronaAI> {
+    await this.ensureProvidersLoaded();
+    return new IronaAI(config);
+  }
+
+  private static async ensureProvidersLoaded(
+    retries = 3,
+    delay = 2000
+  ): Promise<void> {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+      try {
+        await updateProvidersFromGist();
+        return;
+      } catch (error) {
+        console.warn(
+          `Attempt ${attempt} to load providers failed. Retrying...`
+        );
+        if (attempt < retries)
+          await new Promise((res) => setTimeout(res, delay));
+      }
+    }
+    throw new Error(
+      "Failed to load providers from Gist after multiple attempts."
+    );
   }
   public modelSelect(body: ModelSelectPayload): Promise<any> {
     return this.ironaRouter.modelSelect(body);
